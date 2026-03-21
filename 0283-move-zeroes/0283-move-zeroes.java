ac8eaed732de15f1bed765076acd1ee333ad9780
nandini2888 <nandinisaladhi888@gmail.com>
2026-03-21 21:00:20 +0530

    Time: 2 ms (90.55%), Space: 48.1 MB (15.13%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,19 +1,13 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        // Pointer to track the position for the next non-zero element
-        int lastNonZeroFoundAt = 0;
+        int j = 0;  // index for next non-zero element
 
-        // Iterate through the array
-        for (int cur = 0; cur < nums.length; cur++) {
-            // If the current element is NOT zero
-            if (nums[cur] != 0) {
-                // Swap the current element with the 'lastNonZeroFoundAt' element
-                int temp = nums[lastNonZeroFoundAt];
-                nums[lastNonZeroFoundAt] = nums[cur];
-                nums[cur] = temp;
-
-                // Move the pointer forward
-                lastNonZeroFoundAt++;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
             }
         }
     }
