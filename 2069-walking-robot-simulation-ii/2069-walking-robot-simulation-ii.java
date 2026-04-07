1bc5901736e89ebca0fff8c62f798e7066ab3425
nandini2888 <nandinisaladhi888@gmail.com>
2026-04-07 21:32:17 +0530

    Time: 61 ms (77.62%), Space: 58.1 MB (22.38%) - LeetHub

---
+++ 2069-walking-robot-simulation-ii/2069-walking-robot-simulation-ii.java
@@ -0,0 +1,60 @@
+class Robot {
+    int width, height;
+    int x, y;
+    int dir; // 0=East, 1=North, 2=West, 3=South
+    int perimeter;
+
+    public Robot(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.x = 0;
+        this.y = 0;
+        this.dir = 0;
+        this.perimeter = 2 * (width + height) - 4;
+    }
+    
+    public void step(int num) {
+        num %= perimeter;
+
+        // 🔥 Important fix: if num == 0, it means full cycle
+        if (num == 0) num = perimeter;
+
+        while (num > 0) {
+            if (dir == 0) { // East
+                int move = Math.min(num, width - 1 - x);
+                x += move;
+                num -= move;
+                if (move == 0) dir = 1;
+            } 
+            else if (dir == 1) { // North
+                int move = Math.min(num, height - 1 - y);
+                y += move;
+                num -= move;
+                if (move == 0) dir = 2;
+            } 
+            else if (dir == 2) { // West
+                int move = Math.min(num, x);
+                x -= move;
+                num -= move;
+                if (move == 0) dir = 3;
+            } 
+            else { // South
+                int move = Math.min(num, y);
+                y -= move;
+                num -= move;
+                if (move == 0) dir = 0;
+            }
+        }
+    }
+    
+    public int[] getPos() {
+        return new int[]{x, y};
+    }
+    
+    public String getDir() {
+        if (dir == 0) return "East";
+        if (dir == 1) return "North";
+        if (dir == 2) return "West";
+        return "South";
+    }
+}
